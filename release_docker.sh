bbe6fa366ec2e3c4ee07a945da9a64c601ae124f
Daniel Garcia Sanchez <dontic@zoho.com>
2024-10-08 10:40:08 +0000

    chore: Update Docker release script

--- release_docker.sh
+++ release_docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Variables
+DOCKER_REGISTRY="dontic"
+IMAGE_NAME="postifyai"
+
 # Color codes
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -7,25 +11,23 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-# Function to extract the latest release version from git branches
-get_latest_release_version() {
-    git fetch --all >/dev/null 2>&1
-    latest_release=$(git branch -r | grep 'origin/release/' | sed 's/.*\/release\///' | sort -V | tail -n 1)
-    echo "$latest_release"
+# Function to extract the latest version from git tags
+get_latest_version() {
+    git fetch --tags >/dev/null 2>&1
+    latest_tag=$(git tag -l | sort -V | tail -n 1)
+    echo "$latest_tag"
 }
 
-# Get the latest release version
-VERSION=$(get_latest_release_version | tr -d '\n')
-
+# Get the latest version
+VERSION=$(get_latest_version | tr -d '\n')
 if [ -z "$VERSION" ]; then
-    echo -e "${RED}Error: No release branch found.${NC}"
+    echo -e "${RED}Error: No version tags found.${NC}"
     exit 1
 fi
 
 # Set the Docker image name and tags
-IMAGE_NAME="dontic/postifyai"
-VERSION_TAG="${IMAGE_NAME}:${VERSION}"
-LATEST_TAG="${IMAGE_NAME}:latest"
+VERSION_TAG="${DOCKER_REGISTRY}/${IMAGE_NAME}:${VERSION}"
+LATEST_TAG="${DOCKER_REGISTRY}/${IMAGE_NAME}:latest"
 
 # Build the Docker image
 echo -e "${BLUE}Building Docker image: ${YELLOW}$VERSION_TAG${NC}"
